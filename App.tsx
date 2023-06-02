e5d025801d3d8d93ade899f8d47b76e6aa541506
bruno santos <bruno.santos6@ftc.edu.br>
2023-06-02 00:59:15 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -13,33 +13,45 @@ import {
   Image,
   Modal,
 } from 'react-native';
+
 import { PermissionsAndroid, Platform } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
 import { CameraRoll } from '@react-native-camera-roll/camera-roll';
 
-
-
+// Define um componente App que será o componente raiz do aplicativo
 const App = () => {
+  // Cria uma referência para o componente Camera usando o hook useRef
   const camera = useRef<Camera>(null);
+
+  // Usa o hook useCameraDevices para obter uma lista de dispositivos de câmera disponíveis
   const devices = useCameraDevices();
+
+  // Usa o hook useState para criar um estado para controlar a visibilidade do modal
   const [isModalVisible, setIsModalVisible] = useState(false);
+
+  // Usa o hook useState para criar um estado para armazenar o URI da foto tirada
   const [photoUri, setPhotoUri] = useState<string | undefined>(undefined);
+
+  // Seleciona o dispositivo de câmera traseira da lista de dispositivos disponíveis
   const device = devices.back;
 
-
+  // Define uma função assíncrona para verificar se o aplicativo tem permissão para acessar a galeria de fotos no Android
   async function hasAndroidPermission() {
+    // Verifica a versão da plataforma Android e seleciona a permissão apropriada
     const permissionRoll = parseInt(Platform.Version as string, 10) >= 33 ? PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES : PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
 
-      const hasPermission = await PermissionsAndroid.check(permissionRoll);
-      if (hasPermission) {
-        return true;
-      }
-
-      const status = await PermissionsAndroid.request(permissionRoll);
-      return status === 'granted';
+    // Verifica se o aplicativo já tem a permissão necessária
+    const hasPermission = await PermissionsAndroid.check(permissionRoll);
+    if (hasPermission) {
+      return true;
     }
 
+    // Solicita a permissão ao usuário
+    const status = await PermissionsAndroid.request(permissionRoll);
+    return status === 'granted';
+  }
 
+  // Usa o hook useEffect para solicitar permissão para acessar a câmera quando o componente é montado
   useEffect(()=>{
     async function getPermission() {
       const permission = await Camera.requestCameraPermission();
@@ -51,6 +63,7 @@ const App = () => {
     getPermission();
   },[]);
 
+  // Define uma função assíncrona para tirar uma foto com a câmera e salvar na galeria do dispositivo
   const takePicture = async () => {
     try {
       if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
@@ -67,10 +80,13 @@ const App = () => {
       console.error(error);
     }
   };
+
+  // Verifica se um dispositivo de câmera está disponível
   if (device == null){
     return <Text>sem dispositivo</Text>;
   }
 
+  // Renderiza a interface do usuário do aplicativo
   return (
     <SafeAreaView>
       <Camera
@@ -88,9 +104,7 @@ const App = () => {
       </TouchableOpacity>
       {photoUri && (
         <>
-          <TouchableOpacity
-            onPressIn={()=>setIsModalVisible(true)}
-          >
+          <TouchableOpacity onPress={()=>setIsModalVisible(true)}>
             <Image source={{ uri: photoUri }} style={styles.thumbnail} />
           </TouchableOpacity>
           <Modal
